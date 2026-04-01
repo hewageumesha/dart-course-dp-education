bed4a905ce186b29c961ba15aaa33c719b1c6ec7
dil <n.u.m.hewage@gmail.com>
2026-04-01 22:48:00 +0530

    for loop in Dart

---
+++ conditional statements/for_loop.dart
@@ -0,0 +1,44 @@
+void main() {
+  // print hello 10 times
+  for (int i = 0; i < 10; i++) {
+    print('Hello $i');
+  }
+
+  for (int i = 0; i < 10; i += 2) {
+    print('Hello $i');
+  }
+
+  // calculate the sum from 1 to 10 using for loop
+  int sum = 0;
+  for (int i = 1; i < 100; i++) {
+    sum = sum + i;
+  }
+  print(sum);
+
+  // using dart for loop display even numbers in the range(20 to 200)
+  List<int> evenNumbers = [];
+  List<int> oddNumbers = [];
+  for (int i = 20; i <= 200; i++) {
+    if (i % 2 == 0) {
+      evenNumbers.add(i);
+    } else {
+      oddNumbers.add(i);
+    }
+  }
+  print('Even Numbers: $evenNumbers');
+  print('Odd Numbers: $oddNumbers');
+
+  // display all number which are divide by all 3, 5, and 7 from 1 to 1000
+  List<int> numbers = [];
+  for (int i = 1; i <= 1000; i++) {
+    if (i % 3 == 0 && i % 5 == 0 && i % 7 == 0) {
+      numbers.add(i);
+    }
+  }
+  print('Numbers: $numbers');
+
+  String title = 'Flutter and Dart';
+  for (int i = 0; i < title.length; i++) {
+    print(title[i]);
+  }
+}
