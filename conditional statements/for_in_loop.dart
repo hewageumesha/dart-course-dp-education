2db3b32fbde0b4104ceacfb245b16a3fd4f1c827
dil <n.u.m.hewage@gmail.com>
2026-04-08 12:51:55 +0530

    for in loop in Dart

---
+++ conditional statements/for_in_loop.dart
@@ -0,0 +1,22 @@
+void main() {
+  List<int> numbers = [1, 2, 3, 4, 5];
+  for (int number in numbers) {
+    print(number);
+  }
+  // output:
+  // 1
+  // 2
+  // 3
+  // 4
+  // 5
+
+  List<String> fruits = ['apple', 'banana', 'orange'];
+  for (String fruit in fruits) {
+    print(fruit);
+  }
+  // output:
+  // apple
+  // banana
+  // orange
+
+}
