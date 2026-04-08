bb43f6e7778deed94a74846b8cfeca97d6e51f81
dil <n.u.m.hewage@gmail.com>
2026-04-08 21:38:00 +0530

    Introduction to Functions in Dart

---
+++ function/function.dart
@@ -0,0 +1,16 @@
+void main() {
+  // create a function that print hello world
+  void printHelloWorld() {
+    print('Hello World');
+  }
+  printHelloWorld();
+
+  // create a function that takes two parameters and print them
+  void printDetails(String name, int age) {
+    print('$name you are $age years old.');
+  }
+  printDetails('John', 30);
+  printDetails('Jane', 25);
+
+
+}
