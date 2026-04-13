9f59ff6d610b5f226fccade35739fcfd8a1e0e0c
dil <n.u.m.hewage@gmail.com>
2026-04-13 22:40:27 +0530

    Constructors in Dart

--- OOP/dog_class.dart
+++ OOP/dog_class.dart
@@ -4,6 +4,14 @@ class Dog {
   String? breed;
   double? weight;
 
+  // constructor
+  Dog(String name, String breed, double? weight) {
+    this.name = name;
+    this.breed = breed;
+    this.weight = weight;
+    print('Parameterized constructor called');
+  }
+
   //methods
   void bark() {
     print('woof! woof!');
@@ -18,16 +26,18 @@ class Dog {
 
 void main() {
   // creating objects from the class
-  Dog dog1 = Dog();
-  dog1.name = 'Tom';
-  dog1.breed = 'German Shepherd';
-  dog1.weight = null;
+  Dog dog1 = Dog('Tom', 'German Shepherd', null); // called constructor 
+  // Dog dog1 = Dog();
+  // dog1.name = 'Tom';
+  // dog1.breed = 'German Shepherd';
+  // dog1.weight = null;
   dog1.printInfo();
   dog1.bark();
 
-  Dog dog2 = Dog();
-  dog2.name = 'Gimmi';
-  dog2.breed = 'German Shepherd';
-  dog2.weight = 120;
+  Dog dog2 = Dog('Gimmi', 'German Shepherd', 120); // called constructor 
+  // Dog dog2 = Dog();
+  // dog2.name = 'Gimmi';
+  // dog2.breed = 'German Shepherd';
+  // dog2.weight = 120;
   dog2.printInfo();
 }
