0c03f26e31228923857181263edc6fb8986ef9d7
dil <n.u.m.hewage@gmail.com>
2026-04-24 13:57:43 +0530

    Mobile Application Layouts Development using Flutter - Example 01

---
+++ layout_design/lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Flutter Layouts",
+      home: Scaffold(
+        appBar: AppBar(backgroundColor: const Color(0xFF9E00FF)),
+        body: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            children: [
+              Expanded(
+                child: Container(
+                  width: double.infinity,
+                  color: Color(0xFF06FFA5),
+                ),
+              ),
+              SizedBox(height: 10),
+              Expanded(
+                child: Container(
+                  width: double.infinity,
+                  color: Color(0xFFFFE500),
+                ),
+              ),
+            ],
+          ),
+        ),
+      )
+    );
+  }
+}
